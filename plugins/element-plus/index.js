5e4649e905d6ed80e8e7dd12d05e97f7d929ca93
25hours886 <1094662341@qq.com>
2023-09-28 15:53:35 +0800

    更新页面布局
    Signed-off-by: 25hours886 <1094662341@qq.com>

--- plugins/element-plus/index.js
+++
@@ -1,6 +0,0 @@
-import ElementPlus from "element-plus";
-export function loadElementPlus(app) {
-    /** Element Plus 组件完整引入 */
-    app.use(ElementPlus);
-}
-//# sourceMappingURL=index.js.map
--- plugins/index.js
+++
@@ -1,9 +0,0 @@
-import { loadElementPlus } from "./element-plus";
-import { loadElementPlusIcon } from "./element-plus-icon";
-import { loadVxeTable } from "./vxe-table";
-export function loadPlugins(app) {
-    loadElementPlus(app);
-    loadElementPlusIcon(app);
-    loadVxeTable(app);
-}
-//# sourceMappingURL=index.js.map
